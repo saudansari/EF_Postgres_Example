ea491bc308122cd5fa90d2261d69fe9b13abfc3d
LAPTOP-SAUD\saudansari <saud_ansari20@yahoo.com>
2017-11-13 18:31:38 +1100

    Sample App to Connect to PostGresDB

---
+++ EF_Example/EF_Example/Program.cs
@@ -0,0 +1,24 @@
+﻿using Npgsql;
+using System;
+
+namespace EF_Example
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var connString = "Host=localhost;Username=user;Password=password123;Database=user;Pooling=true;";
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+
+                using (var command = new NpgsqlCommand("select * from sample_table", conn))
+                using (var reader = command.ExecuteReader())
+                    while (reader.Read())
+                        Console.WriteLine(reader.GetString(0) + "," + reader.GetString(1));
+
+            }
+        }
+    }
+}
